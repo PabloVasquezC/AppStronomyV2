309faee8dab0d0308eb5c050ee9f8288072fcfce
nicolas290 <nicolasmunososses@gmail.com>
2025-01-07 17:27:19 -0300

    home page v1

--- app/src/main/java/com/example/appstronomyv2/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/appstronomyv2/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Api de la nasa ");
     }
 
     public LiveData<String> getText() {
