3231879213891493aff1bddd744194f96df65901
PabloVasquez <vascor.pablo@gmail.com>
2025-01-15 20:11:12 -0300

    actually changing title o home

--- app/src/main/java/com/example/appstronomyv2/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/appstronomyv2/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Api de la nasa ");
+        mText.setValue("AppStronomy");
     }
 
     public LiveData<String> getText() {
